d810395e0d9490c6d1805bbda45fdaa160b7ad4a
sonlam <19522133@gm.uit.edu.vn>
2022-11-27 02:30:36 +0700

    add api to user, keyword, question

--- src/containers/KeywordManagement/index.jsx
+++ src/containers/KeywordManagement/index.jsx
@@ -1,35 +1,46 @@
-import { Button, Modal, Table } from 'antd';
+import { Button, Pagination, Table } from 'antd';
 import React, { useEffect, useState } from 'react';
 import './styles.scss';
-import { BsTrash } from 'react-icons/bs';
-import { BiEdit } from 'react-icons/bi';
-import KeywordActionModal from './KeywordActionModal';
+import { BsPlusLg, BsTrash } from 'react-icons/bs';
+import { BiEdit, BiSearchAlt2 } from 'react-icons/bi';
+import AddNewModal from './AddNewModal';
+import DeleteModal from './DeleteModal';
 import axios from 'axios';
 
 const KeywordManagement = () => {
   const [isShowEditModal, setIsShowEditModal] = useState(false);
+  const [isShowDeleteModal, setIsShowDeleteModal] = useState(false);
   const [isShowAddModal, setIsShowAddModal] = useState(false);
-  const [listMusics, setListMusics] = useState([]);
+  const [editField, setEditField] = useState();
+  const [listKeywords, setListKeywords] = useState([]);
+  const [searchInput, setSearchInput] = useState('');
+  const [page, setPage] = useState(0);
 
-  const getAllEmotions = async () => {
+  const getAllEmotionsAPI = async () => {
     try {
-      const result = await axios.get('http://localhost:5000/api/emotion');
-
-      if (result.data.emotions) {
-        setListMusics(result.data.emotions);
-      }
+      const result = await axios.post('http://localhost:5000/api/emotion', {
+        name: searchInput,
+      });
+      setListKeywords(result.data.emotions);
     } catch (error) {
-      console.log('login error:', error.response);
+      console.log('error:');
     }
   };
 
   useEffect(() => {
-    getAllEmotions();
-  }, []);
+    getAllEmotionsAPI();
+  }, [searchInput]);
 
   const handleDeleteMusic = (record) => {
-    console.log(record);
+    setEditField(record);
+    setIsShowDeleteModal(true);
   };
+
+  const handleEditClick = (record) => {
+    setEditField(record);
+    setIsShowEditModal(true);
+  };
+
   const columns = [
     {
       title: '#',
@@ -51,21 +62,39 @@ const KeywordManagement = () => {
 
       render: (text, record, index) => (
         <div className='flex items-center gap-2'>
+          <BiEdit
+            className='cursor-pointer hover:scale-150'
+            onClick={() => handleEditClick(record)}
+          />
           <BsTrash
             className='cursor-pointer hover:scale-150'
             onClick={() => handleDeleteMusic(record)}
-          />
-          <BiEdit
-            className='cursor-pointer hover:scale-150'
-            onClick={() => setIsShowEditModal(!isShowEditModal)}
           />
         </div>
       ),
     },
   ];
 
+  const handleEditOk = () => {
+    getAllEmotionsAPI();
+    setIsShowEditModal(false);
+  };
+
+  const handleEditCancel = () => {
+    setIsShowEditModal(false);
+  };
+
+  const handleDeleteSuccess = () => {
+    getAllEmotionsAPI();
+    setIsShowDeleteModal(false);
+  };
+
+  const handleCancelDelete = () => {
+    setIsShowDeleteModal(false);
+  };
+
   const handleAddOk = () => {
-    getAllEmotions();
+    getAllEmotionsAPI();
     setIsShowAddModal(false);
   };
 
@@ -79,14 +108,47 @@ const KeywordManagement = () => {
 
   return (
     <div>
-      <Button type='primary' size='large' onClick={handleOpenAddModal}>
-        Add new
-      </Button>
-      <Table columns={columns} dataSource={listMusics} />
-      <KeywordActionModal
+      <div className='flex justify-between'>
+        <div className='flex gap-1 items-center w-[500px] bg-[#413158] px-2 rounded py-1'>
+          <BiSearchAlt2 className='text-[24px] text-white' />
+          <input
+            type='text'
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+            placeholder='Search....'
+            className='bg-transparent outline-none border-0 text-white'
+          />
+        </div>
+        <div className='flex justify-end'>
+          <Button
+            type='primary'
+            className='bg-white text-primary flex items-center gap-2'
+            size='large'
+            onClick={handleOpenAddModal}
+          >
+            <BsPlusLg />
+            Add new
+          </Button>
+        </div>
+      </div>
+      <Table columns={columns} dataSource={listKeywords ? listKeywords : []} />
+
+      <AddNewModal
+        isShow={isShowEditModal}
+        onOk={handleEditOk}
+        onCancel={handleEditCancel}
+        editField={editField}
+      />
+      <AddNewModal
         isShow={isShowAddModal}
         onOk={handleAddOk}
         onCancel={handleAddCancel}
+      />
+      <DeleteModal
+        isShow={isShowDeleteModal}
+        item={editField}
+        onCancel={handleCancelDelete}
+        onSuccess={handleDeleteSuccess}
       />
     </div>
   );
